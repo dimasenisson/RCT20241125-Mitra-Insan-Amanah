d8a7cad5d977d8c753abf8dbbea4cb6eb3db5481
dimassdoubles <codedimas@gmail.com>
2024-05-21 20:57:10 +0700

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,140 @@
+export const companyName = "PT Mitra Insan Amanah"
+
+export const menus = [
+    {
+        id: "#home",
+        label: "Home",
+    },
+    {
+        id: "#tentang-kami",
+        label: "Tentang Kami",
+    },
+    {
+        id: "#visi",
+        label: "Visi",
+    },
+    {
+        id: "#misi",
+        label: "Misi",
+    },
+    {
+        id: "#layanan",
+        label: "Layanan",
+    },
+    {
+        id: "#klien",
+        label: "Klien",
+    },
+    {
+        id: "#kontak",
+        label: "Kontak",
+    },
+    {
+        id: "#misi",
+        label: "Misi",
+    },
+    
+]
+
+export const heros = [
+    {
+        id: 1,
+        title: "Logistik yang Terpercaya",
+        subtitle: "Subtitle logistik yang terpercaya",
+        image: "",
+    },
+    {
+        id: 2,
+        title: "Pengiriman yang Aman",
+        subtitle: "Subtitle pengiriman yang aman",
+        image: "",
+    },
+]
+
+export const abouts = [
+    'Di dunia yang serba terhubung ini, keandalan dan keamanan dalam layanan logistik sangatlah penting. PT Mitra Insan Amanah hadir sebagai mitra yang dapat diandalkan untuk memenuhi kebutuhan logistik Anda. Dengan tagline kami, "Logistik yang Terpercaya, Pengiriman yang Aman, Bersama Mitra Insan Amanah," kami menekankan komitmen kami untuk memberikan layanan logistik yang dapat dipercaya.',
+    'Kami memahami bahwa bisnis Anda membutuhkan pengiriman barang yang tepat waktu, terjamin keamanannya, serta dilakukan dengan kehati-hatian dan profesionalisme yang tinggi. Mitra Insan Amanah hadir untuk memberikan solusi logistik yang dapat dipercaya, sehingga Anda dapat fokus pada bisnis inti Anda.',
+    'Kami siap menjadi mitra yang dapat Anda andalkan dalam memenuhi kebutuhan logistik Anda. Dengan Mitra Insan Amanah, Anda dapat yakin bahwa layanan logistik yang terpercaya dan pengiriman yang aman akan menjadi prioritas utama kami.',
+]
+
+export const visi = "Menjadi mitra logistik terdepan yang memberikan solusi terpercaya dan inovatif dalam menghubungkan seluruh wilayah Indonesia, mendorong pertumbuhan bisnis, dan memenuhi kebutuhan pelanggan dengan keunggulan operasional."
+
+export const missions = [
+    {
+      id: "0",
+      title: "Memberikan layanan logistik yang terpercaya dan aman",
+      text: "Untuk pengiriman barang ke seluruh Indonesia.",
+    },
+    {
+      id: "1",
+      title: "Mengoptimalkan infrastruktur dan teknologi terkini",
+      text: "Dalam operasional logistik untuk memastikan kecepatan dan efisiensi pengiriman.",
+    },
+    {
+      id: "2",
+      title: "Menjaga kepuasan pelanggan",
+      text: "Dengan menyediakan solusi logistik yang sesuai dengan kebutuhan bisnis mereka.",
+    },
+    {
+      id: "3",
+      title: "Mengembangkan kemitraan yang kuat",
+      text: "dengan pelanggan, pemasok, dan mitra bisnis lainnya untuk saling mendukung dan memperluas jaringan logistik."
+    },
+    {
+      id: "4",
+      title: "Menerapkan praktik bisnis yang berkelanjutan dan ramah lingkungan",
+      text: "dalam setiap aspek operasional logistik.",
+    },
+    {
+      id: "5",
+      title: "Mengedepankan integritas, profesionalisme, dan dedikasi tinggi",
+      text: "dalam setiap layanan yang kami berikan.",
+    },
+    {
+      id: "6",
+      title: "Terus berinovasi dan mengembangkan kemampuan kami untuk tetap menjadi pemimpin",
+      text: "dalam industri logistik di Indonesia.",
+    },
+  ]
+
+export const services = [
+    {
+        id: 1,
+        title: "Pengiriman Reguler",
+        text: "Layanan pengiriman reguler adalah solusi ideal untuk pengiriman barang dengan waktu yang lebih fleksibel. Kami mengatur pengiriman sesuai jadwal rutin yang telah ditentukan. Layanan ini cocok untuk kebutuhan pengiriman yang tidak mendesak namun tetap mengutamakan efisiensi.",
+        image: "",
+    },
+    {
+        id: 2,
+        title: "Trucking",
+        text: "Layanan trucking menyediakan solusi pengiriman barang melalui jaringan jalan darat. Kami memiliki armada truk yang andal dan berpengalaman untuk mengangkut barang dengan cepat dan aman. Layanan ini mencakup pengiriman dalam skala lokal, regional, dan nasional, dengan pilihan truk yang sesuai dengan jenis dan volume barang yang dikirim.",
+        image: "",
+    },
+    {
+        id: 3,
+        title: "Pengiriman Express",
+        text: "Layanan pengiriman ekspres dirancang untuk kebutuhan pengiriman barang yang memerlukan waktu pengiriman cepat dan prioritas tinggi. Kami menjamin pengiriman dengan waktu transit minimal, memungkinkan pelanggan mengirimkan barang dengan kecepatan dan efisiensi maksimal, terutama dalam situasi di mana waktu sangat penting.",
+        image: "",
+    },
+    {
+        id: 4,
+        title: "Container",
+        text: "Layanan pengiriman container memungkinkan pelanggan mengirim barang dalam jumlah besar menggunakan kontainer khusus. Kami menyediakan pengiriman kontainer yang aman dan efisien, baik untuk kebutuhan dalam negeri maupun internasional. Layanan ini mencakup pengangkutan, penanganan, dan pemantauan kontainer secara menyeluruh untuk memastikan barang tiba dengan selamat dan tepat waktu.",
+        image: "",
+    },
+]
+
+export const clients = []
+
+export const addresses = [
+    {
+        id: 1,
+        name: "Office",
+        text: "Graha Krama Yudha Lt. 4 Unit B Jl. Warung Jati Barat No. 43 Jakarta Selatan 12760 Indonesia",
+    },
+    {
+        id: 2,
+        name: "Warehouse",
+        text: "Jl. Taman Harapan Indah Blok HO2 No. 33, Pusaka Rakyat, Kec. Tarumajaya, Kabupaten Bekasi, Jawa Barat 17214",
+    },
+]
