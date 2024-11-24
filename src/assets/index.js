7efb372b036aa635cf637efc554600444f8b7d2d
dimassdoubles <codedimas@gmail.com>
2024-11-25 06:33:25 +0700

    feat: optimize image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,48 +1,48 @@
-import hero1 from './hero/hero-1.png'
-import hero2 from './hero/hero-2.png'
-import hero3 from './hero/hero-3.png'
+import hero1 from "./hero/hero-1.webp";
+import hero2 from "./hero/hero-2.webp";
+import hero3 from "./hero/hero-3.webp";
 
-import bgAboutUs from './about-us/about-us.png'
+import bgAboutUs from "./about-us/about-us.webp";
 
-import bgMission1 from './missions/mission-1.png'
-import bgMission2 from './missions/mission-2.png'
-import bgMission3 from './missions/mission-3.png'
-import bgMission4 from './missions/mission-4.png'
-import bgMission5 from './missions/mission-5.png'
-import bgMission6 from './missions/mission-6.png'
-import bgMission7 from './missions/mission-7.png'
+import bgMission1 from "./missions/mission-1.webp";
+import bgMission2 from "./missions/mission-2.webp";
+import bgMission3 from "./missions/mission-3.webp";
+import bgMission4 from "./missions/mission-4.webp";
+import bgMission5 from "./missions/mission-5.webp";
+import bgMission6 from "./missions/mission-6.webp";
+import bgMission7 from "./missions/mission-7.webp";
 
-import service1 from './services/service-1.png'
-import service2 from './services/service-2.png'
-import service3 from './services/service-3.png'
-import service4 from './services/service-4.png'
+import service1 from "./services/service-1.webp";
+import service2 from "./services/service-2.webp";
+import service3 from "./services/service-3.webp";
+import service4 from "./services/service-4.webp";
 
-import client1 from './clients/client-1.png'
-import client2 from './clients/client-2.png'
-import client3 from './clients/client-3.png'
-import client4 from './clients/client-4.png'
+import client1 from "./clients/client-1.webp";
+import client2 from "./clients/client-2.webp";
+import client3 from "./clients/client-3.webp";
+import client4 from "./clients/client-4.webp";
 
-import logo from './logo.png'
+import logo from "./logo.png";
 
 export {
-    logo,
-    hero1,
-    hero2,
-    hero3,
-    bgAboutUs,
-    bgMission1,
-    bgMission2,
-    bgMission3,
-    bgMission4,
-    bgMission5,
-    bgMission6,
-    bgMission7,
-    service1,
-    service2,
-    service3,
-    service4,
-    client1,
-    client2,
-    client3,
-    client4,
-}
+  logo,
+  hero1,
+  hero2,
+  hero3,
+  bgAboutUs,
+  bgMission1,
+  bgMission2,
+  bgMission3,
+  bgMission4,
+  bgMission5,
+  bgMission6,
+  bgMission7,
+  service1,
+  service2,
+  service3,
+  service4,
+  client1,
+  client2,
+  client3,
+  client4,
+};
